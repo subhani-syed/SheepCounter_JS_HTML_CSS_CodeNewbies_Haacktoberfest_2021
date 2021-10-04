f6271489fa001deb175cc1078cca9f80668d50a7
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2021-10-04 19:57:33 -0300

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,46 @@
+/* -----------JS ISSUE 1 ---------------- */ 
+/* get the elements with id "count-el" and store it in a variable called *counterEl*
+get the element with ClassName "logResult" and store it in a variable called *logResults*
+*/
+
+//Write your code here
+
+
+
+
+
+
+/*  ------------ JS ISSUE 2* ------------/
+
+let count = 0;
+let log =[];
+
+
+function increment(){
+/* 1.this function shoud increment the variable *count* each time the count button is hit. We are counting sheeps,
+so each time hit the count button the *count* variable has to be increased.
+2. put the content of *count* variable into the textcontent of a variable called "counterEl*
+
+
+    // write your code here
+    
+};
+
+
+/* ------------- JS ISSUE 3 ---------------- */
+
+function save(){
+    log.unshift(count);
+    
+    
+    /* Create a for loop to add the results of the last 5 counts. 
+    We will only keep the last 5, each time a new count is added the previous one will be erased
+    and we will only see the last 5. 
+      1.  Create a for loop that iterates < 5 times. 
+      2. Inside the loop, asign the content of *log[i]* to the textContent of *LogResults[i]*.
+      3. Inside the loop set the textcontent of the variable *counterEl* to 0. (we want it back to zero) 
+      4. Insed the loop set the variable *count* to zero, too;
+
+    
+    // write the for loop here 
+;}
